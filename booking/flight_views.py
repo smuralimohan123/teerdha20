55bed2701f5f5a246c33c2991cb20a4a411c7fd3
smuralimohan123 <160567066+smuralimohan123@users.noreply.github.com>
2024-07-26 17:53:54 +0530

    Update flight_views.py

--- booking/flight_views.py
+++ booking/flight_views.py
@@ -83,10 +83,10 @@ def flights(request):
 
     if request.method=="GET":
         nav=navbar.objects.all()
-        ad=requests.get("http://54.209.189.118:8000/flight_home/")
+        ad=requests.get("http://127.0.0.1:8000/flight_home/")
         res=ad.json()
         k3 = flightfaqs.objects.all()
-        wc = requests.get("http://54.209.189.118:8000/why_choose_lc/")
+        wc = requests.get("http://127.0.0.1:8000/why_choose_lc/")
         wcu = wc.json()
         return render(request,"flight_templates/c.html",{'nav':nav,'res':res,'k3':k3,'wcu':wcu,'k':k})
 
